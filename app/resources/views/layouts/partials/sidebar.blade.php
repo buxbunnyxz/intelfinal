1ea202577cff89d7930a271ba08dea438165bb67
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:29:13 -0400

    Auto-commit on save

--- app/resources/views/layouts/partials/sidebar.blade.php
+++ app/resources/views/layouts/partials/sidebar.blade.php
@@ -2,18 +2,18 @@
     <!-- LOGO -->
     <a href="{{ url('/') }}" class="logo text-center logo-light">
         <span class="logo-lg">
-            <img src="{{ asset('assets/images/logo.png') }}" alt="" height="16">
+            <img src="{{ asset('app/assets/images/logo.png') }}" alt="" height="16">
         </span>
         <span class="logo-sm">
-            <img src="{{ asset('assets/images/logo_sm.png') }}" alt="" height="16">
+            <img src="{{ asset('app/assets/images/logo_sm.png') }}" alt="" height="16">
         </span>
     </a>
     <a href="{{ url('/') }}" class="logo text-center logo-dark">
         <span class="logo-lg">
-            <img src="{{ asset('assets/images/logo-dark.png') }}" alt="" height="16">
+            <img src="{{ asset('app/assets/images/logo-dark.png') }}" alt="" height="16">
         </span>
         <span class="logo-sm">
-            <img src="{{ asset('assets/images/logo_sm_dark.png') }}" alt="" height="16">
+            <img src="{{ asset('app/assets/images/logo_sm_dark.png') }}" alt="" height="16">
         </span>
     </a>
     <div class="h-100" id="leftside-menu-container" data-simplebar="">
