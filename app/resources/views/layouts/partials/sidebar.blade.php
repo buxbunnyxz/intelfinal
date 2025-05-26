d7d7b4c2777f1790d1cb453e002b1b5af85e4aa8
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:22:31 -0400

    Auto-commit on save

---
+++ app/resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="leftside-menu">
+    <!-- LOGO -->
+    <a href="{{ url('/') }}" class="logo text-center logo-light">
+        <span class="logo-lg">
+            <img src="{{ asset('assets/images/logo.png') }}" alt="" height="16">
+        </span>
+        <span class="logo-sm">
+            <img src="{{ asset('assets/images/logo_sm.png') }}" alt="" height="16">
+        </span>
+    </a>
+    <a href="{{ url('/') }}" class="logo text-center logo-dark">
+        <span class="logo-lg">
+            <img src="{{ asset('assets/images/logo-dark.png') }}" alt="" height="16">
+        </span>
+        <span class="logo-sm">
+            <img src="{{ asset('assets/images/logo_sm_dark.png') }}" alt="" height="16">
+        </span>
+    </a>
+    <div class="h-100" id="leftside-menu-container" data-simplebar="">
+        <!--- Sidemenu -->
+        @include('layouts.partials.sidemenu')
+        <div class="clearfix"></div>
+    </div>
+</div>
