42b2d990a385d09cb1af4e21761eac674966ebd3
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:24:11 -0400

    Auto-commit on save

--- app/resources/views/layouts/partials/footer.blade.php
+++ app/resources/views/layouts/partials/footer.blade.php
@@ -1,15 +1,8 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-12">
                 <script>document.write(new Date().getFullYear())</script> © IM - Intelboard.ca
-            </div>
-            <div class="col-md-6">
-                <div class="text-md-end footer-links d-none d-md-block">
-                    <a href="javascript: void(0);">About</a>
-                    <a href="javascript: void(0);">Support</a>
-                    <a href="javascript: void(0);">Contact Us</a>
-                </div>
             </div>
         </div>
     </div>
