5ad7aff4ccfc6fd6aabef90376d1c31ad184bff5
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:31:42 -0400

    Auto-commit on save

--- app/resources/views/layouts/partials/sidemenu.blade.php
+++ app/resources/views/layouts/partials/sidemenu.blade.php
@@ -1,5 +1,4 @@
 <ul class="side-nav">
     <!-- Place all your sidebar <li> content here from original HTML.
-         Replace links with Laravel routes as you build them, e.g. {{ route('dashboard') }} -->
-    <!-- ... -->
+-->
 </ul>
