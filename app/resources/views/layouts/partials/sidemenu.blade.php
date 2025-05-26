93da1405f56653a3ad6607e5caa6aefb4a017027
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:37:18 -0400

    Auto-commit on save

--- app/resources/views/layouts/partials/sidemenu.blade.php
+++ app/resources/views/layouts/partials/sidemenu.blade.php
@@ -1,4 +1,27 @@
 <ul class="side-nav">
-    <!-- Place all your sidebar <li> content here from original HTML.
--->
+    <li class="side-nav-title side-nav-item">Navigation</li>
+
+    <li class="side-nav-item">
+        <a data-bs-toggle="collapse" href="#sidebarDashboards" aria-expanded="false" aria-controls="sidebarDashboards" class="side-nav-link">
+            <i class="uil-home-alt"></i>
+            <span class="badge bg-success float-end">4</span>
+            <span> Dashboards </span>
+        </a>
+        <div class="collapse" id="sidebarDashboards">
+            <ul class="side-nav-second-level">
+                <li>
+                    <a href="dashboard-analytics.html">Analytics</a>
+                </li>
+                <li>
+                    <a href="dashboard-crm.html">CRM</a>
+                </li>
+                <li>
+                    <a href="index.html">Ecommerce</a>
+                </li>
+                <li>
+                    <a href="dashboard-projects.html">Projects</a>
+                </li>
+            </ul>
+        </div>
+    </li>
 </ul>
