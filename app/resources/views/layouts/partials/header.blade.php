895e46a35ea22ade9424dac60aee37862c952eb9
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:31:22 -0400

    Auto-commit on save

--- app/resources/views/layouts/partials/header.blade.php
+++ app/resources/views/layouts/partials/header.blade.php
@@ -1,5 +1,3 @@
 <div class="navbar-custom">
-    <!-- Paste your entire topbar (header) section here.
-         Replace all image links like src="assets/images/..." with {{ asset('assets/images/...') }} -->
-    <!-- ... -->
+
 </div>
