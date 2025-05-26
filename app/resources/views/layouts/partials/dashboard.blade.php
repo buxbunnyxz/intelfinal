887b6bb347ecf485b0a607bbd2abe1e8a410ad65
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:23:17 -0400

    Auto-commit on save

---
+++ app/resources/views/layouts/partials/dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', __('Dashboard'))
+
+@section('page-title')
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box">
+                <div class="page-title-right">
+                    <ol class="breadcrumb m-0">
+                        <li class="breadcrumb-item"><a href="#">Hyper</a></li>
+                        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                        <li class="breadcrumb-item active">Starter</li>
+                    </ol>
+                </div>
+                <h4 class="page-title">Starter</h4>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    <!-- Your dashboard content goes here -->
+@endsection
