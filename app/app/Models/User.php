310222ec2dadcb6ce576c62083507f68054df4ed
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 15:25:13 -0400

    Auto-commit on save

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -2,31 +2,34 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'phone_number',
+        'role',
+        'company_name',
+        'company_id',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -34,15 +37,27 @@ class User extends Authenticatable
     ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Boot the model.
+     */
+    protected static function booted()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        static::creating(function ($user) {
+            // Only assign if not already set
+            if (empty($user->company_id)) {
+                do {
+                    $company_id = str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+                } while (self::where('company_id', $company_id)->exists());
+                $user->company_id = $company_id;
+            }
+        });
     }
 }
