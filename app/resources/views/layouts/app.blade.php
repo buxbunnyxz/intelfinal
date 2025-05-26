b90a1c118174b84d2e6acb3af59b51036c819166
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:30:53 -0400

    Auto-commit on save

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -7,12 +7,12 @@
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
     <meta content="Coderthemes" name="author">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{ asset('app/assets/images/favicon.ico') }}">
+    <link rel="shortcut icon" href="{{ asset('app/public/assets/images/favicon.ico') }}">
 
     <!-- App css -->
-    <link href="{{ asset('app/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css">
-    <link href="{{ asset('app/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="light-style">
-    <link href="{{ asset('app/assets/css/app-dark.min.css') }}" rel="stylesheet" type="text/css" id="dark-style">
+    <link href="{{ asset('app/public/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('app/public/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="light-style">
+    <link href="{{ asset('app/public/assets/css/app-dark.min.css') }}" rel="stylesheet" type="text/css" id="dark-style">
 
     @stack('head')
 </head>
@@ -31,8 +31,8 @@
     </div>
 </div>
 <!-- bundle -->
-<script src="{{ asset('app/assets/js/vendor.min.js') }}"></script>
-<script src="{{ asset('app/assets/js/app.min.js') }}"></script>
+<script src="{{ asset('app/public/assets/js/vendor.min.js') }}"></script>
+<script src="{{ asset('app/public/assets/js/app.min.js') }}"></script>
 @stack('scripts')
 </body>
 </html>
