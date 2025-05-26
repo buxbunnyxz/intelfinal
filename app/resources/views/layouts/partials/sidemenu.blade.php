28758f2a64f1d584a2181d1fb71feab90c2761f7
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-05-26 14:37:24 -0400

    Auto-commit on save

--- app/resources/views/layouts/partials/sidemenu.blade.php
+++ app/resources/views/layouts/partials/sidemenu.blade.php
@@ -12,7 +12,7 @@
                 <li>
                     <a href="dashboard-analytics.html">Analytics</a>
                 </li>
-                <li>xx
+                <li>
                     <a href="dashboard-crm.html">CRM</a>
                 </li>
                 <li>
