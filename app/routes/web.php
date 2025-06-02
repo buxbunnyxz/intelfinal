947620cdc7028558497ab84458fa87b4bddf457d
Ismail Merdjaoui <imerdjaouicad@gmail.com>
2025-06-01 20:48:21 -0400

    Auto-commit on save

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,30 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-//Route::middleware(['auth'])->group(function () {
-//    Route::get('/', function () {
-//        return view('dashboard');
-//    })->name('dashboard');
-//
-//    // Put all your other routes here, or use Route::resource, etc.
-//    // Example:
-//    // Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-//
-//    // For future routes, add them inside this group!
-//});
-
-## Login routes
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-## routes that require being logged in
+// Routes that require being logged in
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {return view('dashboard');})->name('dashboard');
+    Route::get('/', function () { return view('dashboard'); })->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
